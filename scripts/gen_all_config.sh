1bb7b583d0650b7b28fb73c1ebabdf849725fe4b
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-04-26 16:17:13 +0600

    Бэкапы + проверка конфигов

--- scripts/gen_all_config.sh
+++ scripts/gen_all_config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 . config.ini 
+
+rm -rf backup/
+mkdir -p backup/
+mv -f /etc/nginx/conf.d/*.conf backup
+mv -f $mainroot/* backup
+
 for config in "${@:-rkn}"; do
 	echo create /etc/nginx/conf.d/$config.conf
 	./scripts/genconfig \
@@ -11,3 +17,4 @@ for config in "${@:-rkn}"; do
 	mkdir -p $mainroot/$config/
 	cp -v pages/$config.html $mainroot/$config/index.html
 done
+service nginx configtest
